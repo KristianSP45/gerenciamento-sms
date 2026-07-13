488bb71a5d3c1aa2b2c4d7af3fd6d2e52ecbd9e1
Kristian Pessoa <sousakristian45@gmail.com>
2026-07-13 16:00:18 -0300

    Inicio do projeto

---
+++ src/main/java/com/kristian/gerenciamentosms/GerenciamentoSmsApplication.java
@@ -0,0 +1,13 @@
+package com.kristian.gerenciamentosms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoSmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kristian/gerenciamentosms/GerenciamentoSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristian.gerenciamentosms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoSmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
